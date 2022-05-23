f40dca17a8b96e7de2b43a66a6988bd6ad124dcb
tommilostny <tommilostny@live.com>
2022-05-23 19:42:02 +0200

    Updated photo shell script.

--- updateFrame.sh
+++ updateFrame.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 #/usr/bin/raspistill -w 1024 -h 768 -n -q 80 -ex auto -hf -vf -t 1 -o /tmp/frame.jpg
-/usr/bin/raspistill -w 2588 -h 1920 -n -q 80 -br 60 -co 20 -ex auto -hf -vf -t 1 -o /tmp/frame.jpg
+/usr/bin/raspistill -w 2588  -h 1920 -n -q 80 -br 56 -co 20 -ex auto -hf -vf -o /tmp/frame.jpg
